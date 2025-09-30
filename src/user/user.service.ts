b3e8ce46d1c10e1998d61c8cb1fe3b3d50665cae
hainghuu19 <nguyenhuuhai7a@gmail.com>
2025-10-01 01:01:16 +0700

    add rating, contribution, languageUsed and stats fields for api user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,14 +6,58 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async getUser(id: string) {
-    return this.prisma.user.findUnique({
+    const user = await this.prisma.user.findUnique({
       where: { id },
       select: {
         id: true,
         email: true,
         name: true,
         role: true,
+        rating: true,
+        contributions: true,
       },
     });
+
+    if (!user) {
+      return { message: 'User not found' };
+    }
+
+    const submissions = await this.prisma.submission.findMany({
+      where: { userId: id },
+      select: {
+        status: true,
+        problemId: true,
+        language: {
+          select: {
+            name: true,
+          }
+        }
   }
+    });
+
+    const solvedProblems = new Set(
+      submissions.filter(s => s.status === 'ACCEPTED').map(s => s.problemId),
+    );
+
+    const inProgressProblems = new Set(
+      submissions.filter(s => s.status !== 'ACCEPTED').map(s => s.problemId),
+    );
+
+    const langUsage: Record<string, number> = {};
+    submissions.forEach(s => {
+      langUsage[s.language.name] = (langUsage[s.language.name] || 0) + 1;
+    });
+
+    return {
+      ...user,
+      stats: {
+        solved: solvedProblems.size,
+        inProgress: inProgressProblems.size,
+        accepted: solvedProblems.size,
+    },
+    languagesUsed: Object.entries(langUsage).map(([language, count]) => ({ language, count })),
+    };
+    
+  }
+
 }
